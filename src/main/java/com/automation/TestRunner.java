4609cd4b3ff66c305ebe21c7108d3774f5e53ae2
mrtka <murat.karacay@gmail.com>
2020-02-19 19:17:07 +0000

    Mobile Testing

---
+++ src/main/java/com/automation/TestRunner.java
@@ -0,0 +1,42 @@
+package com.automation;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class TestRunner {
+    private AppiumDriver<MobileElement> driver;
+
+    @Test
+    public void test(){
+
+
+        try {
+            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+            desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+            desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.0");
+            desiredCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+ "\\etsy.apk");
+            desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_2_1");
+            desiredCapabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 20000);
+            driver = new AppiumDriver<>(new URL("http://0.0.0.0:4723/wd/hub"), desiredCapabilities);
+            Thread.sleep(3000);
+            WebElement getstarted = driver.findElement(By.xpath("//*[@text='Get Started']"));
+            getstarted.click();
+            WebElement signin = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.EditText[1]\n"));
+
+            signin.sendKeys("fdsf@sdfsd");
+            Thread.sleep(3000);
+            driver.closeApp();
+        } catch (MalformedURLException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
